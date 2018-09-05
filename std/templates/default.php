0c0cf31ddc05ac1e1782505aca5ecd5fe1857ae2
Yves Brisch <yves.brisch@m-pathy.com>
2018-09-05 11:48:13 +0200

    adjusted the default template

--- std/templates/default.php
+++ std/templates/default.php
@@ -11,13 +11,14 @@
           <li><a href = "<?= WEBDIR ?>">Home</a></li>
           <li><a href = "<?= WEBDIR ?>about/">About</a></li>
           <li><a href = "<?= WEBDIR ?>products/">Products</a></li>
-          <li><ol>
+          <li>
+            <a href = "javascript:void(0)">Service</a>
+            <ol>
               <li><a href = "<?= WEBDIR ?>contact/">Contact</a></li>
               <li><a href = "<?= WEBDIR ?>career/">Career</a></li>
               <li><a href = "<?= WEBDIR ?>privacy/">Privacy</a></li>
               <li><a href = "<?= WEBDIR ?>imprint/">Imprint</a></li>
             </ol>
-            <li><a href = "javascript:void(0)">Service</a></li>
           </li>
         </ol>
       </nav>
